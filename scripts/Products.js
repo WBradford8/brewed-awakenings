683aa78c6740d7503d33965e027df266739fc8bf
Wade Bradford <wadebradfordtn@gmail.com>
2021-07-23 09:53:26 -0500

    first commit

---
+++ scripts/Products.js
@@ -0,0 +1,17 @@
+import { getProducts } from "./database.js"
+
+const products = getProducts()
+console.log(products)
+
+export const Products = () => {
+    let html = "<ul>"
+
+    for (const product of products) {
+        html += `<li id="product--${product.id}">${product.name}</li>`
+    }
+
+    html += "</ul>"
+
+    return html
+}
+
